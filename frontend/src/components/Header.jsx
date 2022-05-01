bbf7213ccb07931f3fa10c5ccd37f336554d320b
ta-mu-github <64738234+ta-mu-github@users.noreply.github.com>
2022-05-01 17:26:22 +0900

    ADD_ハンバーガメニューを押下した時の動作

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,17 @@
+import { useState } from 'react';
 import { MdFoodBank } from "react-icons/md"
 import { GiHamburgerMenu } from "react-icons/gi"
 
-export const Header = () => {
+export const Header = (props) => {
+  const [openMenu, setOpenMenu] = useState(false);
+  console.log(openMenu);
+  const data = props.list;
+  console.log(data);
+
+  const menuFunction = () => {
+    setOpenMenu(!openMenu);
+  };  
+
   return (
     <>
       <div class="bg-white lg:pb-12">
@@ -13,24 +23,38 @@ export const Header = () => {
             </a>
 
             <nav class="hidden lg:flex gap-12">
-              <a href="#" class="text-gray-600 hover:text-red-500 active:text-yellow-500 text-lg font-semibold ">
-                ほげほげ１
-              </a>
-
-              <a href="#" class="text-gray-600 hover:text-red-500 active:text-yellow-700 text-lg font-semibold ">
-                ほげほげ２
-              </a>
-
-              <a href="#" class="text-gray-600 hover:text-red-500 active:text-yellow-700 text-lg font-semibold">
-                ほげほげ３
-              </a>
-
-              <a href="#" class="text-gray-600 hover:text-red-500 active:text-yellow-700 text-lg font-semibold">
-                ほげほげ４
-              </a>
+              <a href="#" class="text-gray-600 hover:text-red-500 active:text-yellow-500 text-lg font-semibold ">about</a>
+              <a href="#" class="text-gray-600 hover:text-red-500 active:text-yellow-700 text-lg font-semibold ">blog</a>
+              <a href="#" class="text-gray-600 hover:text-red-500 active:text-yellow-700 text-lg font-semibold">hogehoge</a>
             </nav>
 
-            <button type="button" class="inline-flex items-center lg:hidden bg-gray-200 hover:bg-gray-300 focus-visible:ring ring-indigo-300 text-gray-500 active:text-gray-700 text-sm md:text-base font-semibold rounded-lg gap-2 px-2.5 py-2">
+
+            {openMenu ? (
+            <div className='flex flex-row absolute z-10 top-0 right-0  min-h-fit min-w-full'>
+              <div className='basis-1/2'></div>
+
+              <div className='basis-1/2 bg-white'>
+                <ul className=' text-center border-l-2 '>
+                  <li className='p-2 border-b-2'>
+                    <button onClick={menuFunction} className='font-bold'>
+                      close
+                    </button>
+                  </li>
+                  <li className='p-2 border-b-2'>
+                    <a href="#" class="text-gray-600 hover:text-red-500 active:text-yellow-500 text-lg font-semibold ">about</a>
+                  </li>
+                  <li className='p-2 border-b-2'>
+                    <a href="#" class="text-gray-600 hover:text-red-500 active:text-yellow-700 text-lg font-semibold ">blog</a>
+                  </li>
+                  <li className='p-2 border-b-2'>
+                    <a href="#" class="text-gray-600 hover:text-red-500 active:text-yellow-700 text-lg font-semibold">hogehoge</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          ) : undefined}
+
+            <button type="button" onClick={menuFunction} class="inline-flex items-center lg:hidden bg-gray-200 hover:bg-gray-300 focus-visible:ring ring-indigo-300 text-gray-500 active:text-gray-700 text-sm md:text-base font-semibold rounded-lg gap-2 px-2.5 py-2">
               <label className="text-2xl text-gray-600"><GiHamburgerMenu /></label>
             </button>
           </header>
