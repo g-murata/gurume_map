3d58a8d74980bef767e57f6776e748267e66f75f
ta-mu-github <64738234+ta-mu-github@users.noreply.github.com>
2022-05-01 16:51:20 +0900

    MOD_レスポンシブにしてみる。

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -1,35 +1,40 @@
 export const Main = () => {
   return (
     <>
-      <div class="max-w-screen-2xl px-4 md:px-8 mx-auto">
+      <div class="max-w-screen-2xl px-4 md:px-8 mx-auto lg:flex lg:space-x-8">
+
         <div class="max-w-sm rounded overflow-hidden shadow-lg 
-      transform hover:scale-110 transition-transform">
+              transform hover:scale-110 transition-transform">
           <img
-            class="w-full"
-            src="https://source.unsplash.com/random/1600x900/"
-            alt="Sunset in the mountains"
-          ></img>
-          <div class="px-6 py-4">
-            <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
-            <p class="text-gray-700 text-base">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Voluptatibus quia, nulla! Maiores et perferendis eaque,
-              exercitationem praesentium nihil.
-            </p>
-          </div>
-          <div class="px-6 py-4">
-            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-              #photography
-            </span>
-            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-              #travel
-            </span>
-            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">
-              #winter
-            </span>
-          </div>
+              class="w-full"
+              src="https://source.unsplash.com/random/1600x900/"
+              alt="ほげほげ画像"
+            ></img>          
+            <div class="px-6 py-4">
+              <div class="font-bold text-xl mb-2">ほげほげタイトル</div>
+              <p class="text-gray-700 text-base">
+              ほげほげ本文
+              </p>
+            </div>
         </div>
+
+        <div class="max-w-sm rounded overflow-hidden shadow-lg 
+              transform hover:scale-110 transition-transform">
+          <img
+              class="w-full"
+              src="https://source.unsplash.com/random/1600x900/"
+              alt="ほげほげ画像"
+            ></img>          
+            <div class="px-6 py-4">
+              <div class="font-bold text-xl mb-2">ほげほげタイトル</div>
+              <p class="text-gray-700 text-base">
+              ほげほげ本文
+              </p>
+            </div>
+        </div>
+
       </div>
+      
     </>
   )
 }
