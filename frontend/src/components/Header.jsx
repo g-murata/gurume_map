80ada56f92e1184edb0e55d281b890c232391b5d
ta-mu-github <64738234+ta-mu-github@users.noreply.github.com>
2022-05-05 10:03:59 +0900

    MOD_トップページのリンクもaからLinkに変更

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -19,10 +19,10 @@ export const Header = (props) => {
       <div class="bg-white lg:pb-12">
         <div class="max-w-screen-2xl px-4 md:px-8 mx-auto">
           <header class="flex justify-between items-center py-4 md:py-8">
-            <a href="/" class="inline-flex items-center text-black-800 text-2xl md:text-3xl font-bold gap-2.5">
+            <Link to="/" class="inline-flex items-center text-black-800 text-2xl md:text-3xl font-bold gap-2.5">
               GurumeMap
               <label className="text-5xl text-red-600"><MdFoodBank /></label>
-            </a>
+            </Link>
             
             <nav class="hidden lg:flex gap-12">    
               <Link to="/about" class="text-gray-600 hover:text-red-500 active:text-yellow-700 text-lg font-semibold ">about</Link>            
