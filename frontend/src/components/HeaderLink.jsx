bcc33700984a9cf988c26e24702582ce91f9cb95
ta-mu-github <64738234+ta-mu-github@users.noreply.github.com>
2022-05-05 17:10:22 +0900

    MOD_Mainの内容をBlogコンポーネントに移動

--- frontend/src/components/HeaderLink.jsx
+++ frontend/src/components/HeaderLink.jsx
@@ -4,13 +4,13 @@ export const HeaderLink = () => {
   return (
     <>      
       <li className='p-2 border-b-2 list-none lg:border-none'>
-        <Link to="/about" class="text-gray-600 hover:text-red-500 active:text-yellow-700 text-lg font-semibold block">about</Link>
+        <Link to="/about" class="text-gray-600 hover:text-red-500 active:text-yellow-700 text-lg font-semibold block">GurumeMapとは</Link>
       </li>             
       <li className='p-2 border-b-2 list-none lg:border-none'>
-        <Link to="/blog" class="text-gray-600 hover:text-red-500 active:text-yellow-700 text-lg font-semibold block">blog</Link>
+        <Link to="/blog" class="text-gray-600 hover:text-red-500 active:text-yellow-700 text-lg font-semibold block">開発者ブログ</Link>
       </li>  
       <li className='p-2 border-b-2 list-none lg:border-none'>
-        <Link to="/hogehoge" class="text-gray-600 hover:text-red-500 active:text-yellow-700 text-lg font-semibold block">hogehoge</Link>
+        <Link to="/hogehoge" class="text-gray-600 hover:text-red-500 active:text-yellow-700 text-lg font-semibold block">ほげほげ</Link>
       </li>  
     </>
 )
--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -1,40 +1,7 @@
 export const Main = () => {
   return (
     <>
-      <div class="flex items-center flex-col max-w-screen-2xl px-4 md:px-8 mx-auto lg:flex-row lg:space-x-8">
-
-        <div class="max-w-sm rounded overflow-hidden shadow-lg 
-              transform hover:scale-110 transition-transform cursor-pointer">
-          <img
-              class="w-full"
-              src="https://source.unsplash.com/random/1600x900/"
-              alt="ほげほげ画像"
-            ></img>          
-            <div class="px-6 py-4">
-              <div class="font-bold text-xl mb-2">ほげほげタイトル</div>
-              <p class="text-gray-700 text-base">
-              ほげほげ本文
-              </p>
-            </div>
-        </div>
-
-        <div class="max-w-sm rounded overflow-hidden shadow-lg 
-              transform hover:scale-110 transition-transform cursor-pointer">
-          <img
-              class="w-full"
-              src="https://source.unsplash.com/random/1600x900/"
-              alt="ほげほげ画像"
-            ></img>          
-            <div class="px-6 py-4">
-              <div class="font-bold text-xl mb-2">ほげほげタイトル</div>
-              <p class="text-gray-700 text-base">
-              ほげほげ本文
-              </p>
-            </div>
-        </div>
-
-      </div>
-      
+     
     </>
   )
 }
