094e6fc0529616f70ab8a8c1ae5d959f96a9991b
ta-mu-github <64738234+ta-mu-github@users.noreply.github.com>
2022-05-07 17:01:37 +0900

    ADD_ためしに

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -6,6 +6,27 @@ import {
   Marker,
 } from "@react-google-maps/api";
 
+
+import Modal from 'react-modal';
+
+const customStyles = {
+  overlay: {
+    position: "fixed",
+    top: 0,
+    left: 0,
+    backgroundColor: "rgba(0,0,0,0.50)"
+  },  
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',  
+  },
+};
+
+
 const containerStyle = {
   height: "60vh",
   // TODO:widthはスマホの時だけ100%にしたい。
@@ -35,16 +56,28 @@ const divStyle = {
 const url = process.env.REACT_APP_GOOGLE_MAP_API_KEY
 
 export const Main = () => {
+
+  const [modalIsOpen, setIsOpen] = useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function afterOpenModal() {
+    // subtitle.style.color = '#f00';
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+
   const [size, setSize] = useState(undefined);
   const infoWindowOptions = {
     pixelOffset: size,
   };
   const createOffsetSize = () => {
     return setSize(new window.google.maps.Size(0, -45));
-  };
-
-  const testFunction = () => {
-    alert("ここでモーダルを出して詳細を表示させたいのさ");
   };
 
   return (
@@ -58,14 +91,41 @@ export const Main = () => {
               <h1>石井ビル</h1>
             </div>
           </InfoWindow>
+
           <InfoWindow position={positionKankoku} options={infoWindowOptions}>
-            <div style={divStyle} class="cursor-pointer" button onClick={testFunction}>
+            <div style={divStyle} class="cursor-pointer" button onClick={openModal}>
               <h1>ヨプの王豚塩焼</h1>
               <p>飲み会でよく行く。</p>
             </div>
           </InfoWindow>
+          
+          <Modal
+            isOpen={modalIsOpen}
+            onAfterOpen={afterOpenModal}
+            onRequestClose={closeModal}
+            style={customStyles}
+            contentLabel="Example Modal"
+          >
+            <div class="flex place-content-between shadow-lg">
+              <div class="text-3xl font-bold mb-2">ヨプの王豚塩焼</div>
+              <button class="font-bold" onClick={closeModal}>close</button>
+            </div>
+            <img
+                class="w-full"
+                src="https://source.unsplash.com/random/1600x900/"
+                alt="ほげほげ画像"
+              ></img>          
+            <p class="text-gray-700 text-base">
+                <p>ほげほげ本文</p>
+                <p>ほげほげ</p>
+                <p>ほげほげ</p>
+                <p>うましうまし</p>              
+            </p>
+
+          </Modal>     
+
         </GoogleMap>
-      </div>          
+      </div>     
     </LoadScript>
   );
 };
