b24412bbc189622e28d7c17fd1a33ff743d4acf4
ta-mu-github <64738234+ta-mu-github@users.noreply.github.com>
2022-05-07 16:50:43 +0900

    ADD_モーダルテスト

---
+++ frontend/src/components/GurumeModal.jsx
@@ -0,0 +1,72 @@
+import { useState } from 'react';
+import Modal from 'react-modal';
+
+const customStyles = {
+  overlay: {
+    position: "fixed",
+    top: 0,
+    left: 0,
+    backgroundColor: "rgba(0,0,0,0.50)"
+  },  
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',  
+  },
+};
+
+// Make sure to bind modal to your appElement (https://reactcommunity.org/react-modal/accessibility/)
+
+// Modal.setAppElement('#yourAppElement');
+
+export const GurumeModal = () => {
+  let subtitle;
+  const [modalIsOpen, setIsOpen] = useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    subtitle.style.color = '#f00';
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  return (
+    <div>
+      <button onClick={openModal}>モーダルボタン</button>
+      <div class ="bg-black">
+      <Modal
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+          <div class="flex place-content-between shadow-lg">
+            <div class="text-3xl font-bold mb-2">ヨプの王豚塩焼</div>
+            <button class="font-bold" onClick={closeModal}>close</button>
+          </div>
+          <img
+              class="w-full"
+              src="https://source.unsplash.com/random/1600x900/"
+              alt="ほげほげ画像"
+            ></img>          
+          <p class="text-gray-700 text-base">
+              <p>ほげほげ本文</p>
+              <p>ほげほげ</p>
+              <p>ほげほげ</p>
+              <p>うましうまし</p>              
+          </p>
+      </Modal>
+      </div>          
+    </div>
+  );
+}
--- frontend/src/components/HogeHoge.jsx
+++ frontend/src/components/HogeHoge.jsx
@@ -1,9 +1,12 @@
+import { GurumeModal } from "./GurumeModal"
+
 export const HogeHoge = () => {
   return (
     <>
       <div class="max-w-screen-2xl px-4 md:px-8 mx-auto">
         <h1 class="bg-orange-300">Hello,hogehoge</h1>
-      </div>   
+        <GurumeModal />
+      </div>        
     </>
 )
 }
