990b33ff456763624806fefd55dd5aa86b064023
ta-mu-github <64738234+ta-mu-github@users.noreply.github.com>
2022-05-11 22:18:08 +0900

    MOD_選択したモーダルを表示させる仕組み。理解せな。

--- frontend/src/components/GurumeModal.jsx
+++ frontend/src/components/GurumeModal.jsx
@@ -24,49 +24,92 @@ const customStyles = {
 
 export const GurumeModal = () => {
   let subtitle;
-  const [modalIsOpen, setIsOpen] = useState(false);
 
-  function openModal() {
-    setIsOpen(true);
+  const [selectedItem, setSelectedItem] = useState('')
+  const ButtonList = ['石井ビル', 'ヨプの王豚塩焼', '常陸秋そば　善三郎', 'Button4', 'Button5']
+
+
+  const onOpenDialog = (name) => {
+    setSelectedItem(name)
   }
+
+  const onCloseDialog = () => {
+    setSelectedItem(false)
+  }  
 
   function afterOpenModal() {
     // references are now sync'd and can be accessed.
     subtitle.style.color = '#f00';
   }
 
-  function closeModal() {
-    setIsOpen(false);
-  }
-
   return (
-    <div>
-      <button onClick={openModal}>モーダルボタン</button>
-      <div class ="bg-black">
-      <Modal
-        isOpen={modalIsOpen}
-        onAfterOpen={afterOpenModal}
-        onRequestClose={closeModal}
-        style={customStyles}
-        contentLabel="Example Modal"
-      >
-          <div class="flex place-content-between w-11/12  m-auto">
-            <div class="text-3xl font-bold mb-2">ヨプの王豚塩焼</div>
-            <button class="font-bold" onClick={closeModal}>close</button>
-          </div>
-          <img
-              class="w-11/12 m-auto"
-              src="https://source.unsplash.com/random/1600x900/"
-              alt="ほげほげ画像"
-            ></img>          
-          <p class="text-gray-700 text-base w-11/12 m-auto">
-              <p>ほげほげ本文</p>
-              <p>ほげほげ</p>
-              <p>ほげほげ</p>
-              <p>うましうまし</p>              
-          </p>
-      </Modal>
-      </div>          
-    </div>
-  );
+    <>
+        { ButtonList.map((item) => {
+          return(
+            <>
+              <button onClick={ () => onOpenDialog(item)}>{item}</button>
+              <div class ="bg-black"></div>     
+             <Modal
+              isOpen={item === selectedItem}
+              onAfterOpen={afterOpenModal}
+              onRequestClose={onCloseDialog}
+              style={customStyles}
+              contentLabel="Example Modal"
+            >
+                <div class="flex place-content-between w-11/12  m-auto">
+                  <div class="text-3xl font-bold mb-2">{item}</div>
+                  <button class="font-bold" onClick={onCloseDialog}>close</button>
+                </div>
+                <img
+                    class="w-11/12 m-auto"
+                    src="https://source.unsplash.com/random/1600x900/"
+                    alt="ほげほげ画像"
+                  ></img>          
+                <p class="text-gray-700 text-base w-11/12 m-auto">
+                    <p>{item}</p>
+                </p>
+            </Modal>
+            </>
+            )          
+          })}
+    </>
+  )
+
+// { ButtonList.map((item) => {
+//   return (
+//     <>
+//       <div key={item}>
+//         <button onClick={ () => onOpenDialog(item)}>モーダルボタン</button>
+//       </div>        
+//     </>
+
+//       // <button onClick={onOpenDialog}>モーダルボタン</button>
+//       // <div class ="bg-black">
+//       // <Modal
+//       //   isOpen={modalIsOpen}
+//       //   onAfterOpen={afterOpenModal}
+//       //   onRequestClose={closeModal}
+//       //   style={customStyles}
+//       //   contentLabel="Example Modal"
+//       // >
+//       //     <div class="flex place-content-between w-11/12  m-auto">
+//       //       <div class="text-3xl font-bold mb-2">ヨプの王豚塩焼</div>
+//       //       <button class="font-bold" onClick={onCloseDialog}>close</button>
+//       //     </div>
+//       //     <img
+//       //         class="w-11/12 m-auto"
+//       //         src="https://source.unsplash.com/random/1600x900/"
+//       //         alt="ほげほげ画像"
+//       //       ></img>          
+//       //     <p class="text-gray-700 text-base w-11/12 m-auto">
+//       //         <p>ほげほげ本文</p>
+//       //         <p>ほげほげ</p>
+//       //         <p>ほげほげ</p>
+//       //         <p>うましうまし</p>              
+//       //     </p>
+//       // </Modal>
+//       // </div>          
+//     // </div>
+//   )
+//  })}
 }
