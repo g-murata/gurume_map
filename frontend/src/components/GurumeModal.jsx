00dbe0c423f3c246c5273f591f0867725de21106
ta-mu-g-m <jojo1234dorararara@gmail.com>
2022-05-14 17:31:51 +0900

    MOD_jsonを読み込む。（ちょっと感動する。）

--- frontend/src/components/GurumeModal.jsx
+++ frontend/src/components/GurumeModal.jsx
@@ -4,10 +4,9 @@ import Modal from 'react-modal';
 import Data from './../data.json'; // 追加
 import Restaurants from './../restaurants.json'; // 追加
 
-const restaurant = Restaurants.data;
-console.log(restaurant)
-console.log(restaurant[0])
-console.log(Object.keys(restaurant).length)
+const restaurants = Restaurants.data;
+console.log(restaurants)
+console.log(Object.keys(restaurants).length)
 const titleText = Data.data.header.title;
 
 // const descriptionText = Data.data.header.text;
@@ -39,14 +38,8 @@ export const GurumeModal = () => {
 
   const [selectedItem, setSelectedItem] = useState('')
 
-  const restaurants = {
-    id: [1, 2, 3, 4, 5],
-    name: ['ヨプの王豚塩焼', '常陸秋そば　善三郎', 'SHRIMP NOODLE海老ポタ', 'Button4', 'Button5'],
-    evaluation: ['3.5', '3.5', '4.0', '', ''],
-    review: ['焼肉うまし', '卵はINしない方が良い。', 'クリーミーでうまい', '', '']
-  }
-
   const onOpenDialog = (name) => {
+    console.log(name)
     setSelectedItem(name)
   }
 
@@ -61,20 +54,20 @@ export const GurumeModal = () => {
 
   return (
     <>
-      {restaurants.name.map((item) => {
+      {Object.keys(restaurants).map(item => {
         return (
           <>
-            <button onClick={() => onOpenDialog(item)}>{item}</button>
+            <button onClick={() => onOpenDialog(restaurants[item].id)}>{restaurants[item].name}</button>
             <div class="bg-black"></div>
             <Modal
-              isOpen={item === selectedItem}
+              isOpen={restaurants[item].id === selectedItem}
               onAfterOpen={afterOpenModal}
               onRequestClose={onCloseDialog}
               style={customStyles}
               contentLabel="Example Modal"
             >
               <div class="flex place-content-between w-11/12  m-auto">
-                <div class="text-3xl font-bold mb-2">{item}</div>
+                <div class="text-3xl font-bold mb-2">{restaurants[item].name}</div>
                 <button class="font-bold" onClick={onCloseDialog}>close</button>
               </div>
               <img
@@ -83,8 +76,8 @@ export const GurumeModal = () => {
                 alt="ほげほげ画像"
               ></img>
               <p class="text-gray-700 text-base w-11/12 m-auto">
-                <p>評価{titleText}</p>
-                <p>{restaurant}</p>
+                <p>評価{restaurants[item].evaluation}</p>
+                <p>{restaurants[item].review}</p>
               </p>
             </Modal>
           </>
@@ -93,3 +86,37 @@ export const GurumeModal = () => {
     </>
   )
 }
+
+  // return (
+  //   <>
+  //     {Object.keys(restaurants).map(item => {
+  //       return (
+  //         <>
+  //           <button onClick={() => onOpenDialog(item)}>{item}</button>
+  //           <div class="bg-black"></div>
+  //           <Modal
+  //             isOpen={item === selectedItem}
+  //             onAfterOpen={afterOpenModal}
+  //             onRequestClose={onCloseDialog}
+  //             style={customStyles}
+  //             contentLabel="Example Modal"
+  //           >
+  //             <div class="flex place-content-between w-11/12  m-auto">
+  //               <div class="text-3xl font-bold mb-2">{item}</div>
+  //               <button class="font-bold" onClick={onCloseDialog}>close</button>
+  //             </div>
+  //             <img
+  //               class="w-11/12 m-auto"
+  //               src="https://source.unsplash.com/random/1600x900/"
+  //               alt="ほげほげ画像"
+  //             ></img>
+  //             <p class="text-gray-700 text-base w-11/12 m-auto">
+  //               <p>評価{titleText}</p>
+  //               <p>{restaurants}</p>
+  //             </p>
+  //           </Modal>
+  //         </>
+  //       )
+  //       )}}
+  //   </>
+  // )
