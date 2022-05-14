63dd1084cba59d76e1734f385247b4a69ac235d4
ta-mu-g-m <jojo1234dorararara@gmail.com>
2022-05-14 16:54:22 +0900

    ADD_Railsを使うまでは、JSONで代用

--- frontend/src/components/GurumeModal.jsx
+++ frontend/src/components/GurumeModal.jsx
@@ -1,20 +1,32 @@
 import { useState } from 'react';
 import Modal from 'react-modal';
 
+import Data from './../data.json'; // 追加
+import Restaurants from './../restaurants.json'; // 追加
+
+const restaurant = Restaurants.data;
+console.log(restaurant)
+console.log(restaurant[0])
+console.log(Object.keys(restaurant).length)
+const titleText = Data.data.header.title;
+
+// const descriptionText = Data.data.header.text;
+
+
 const customStyles = {
   overlay: {
     position: "fixed",
     top: 0,
     left: 0,
     backgroundColor: "rgba(0,0,0,0.50)"
-  },  
+  },
   content: {
     top: '50%',
     left: '50%',
     right: 'auto',
     bottom: 'auto',
     marginRight: '-50%',
-    transform: 'translate(-50%, -50%)',  
+    transform: 'translate(-50%, -50%)',
   },
 };
 
@@ -28,10 +40,10 @@ export const GurumeModal = () => {
   const [selectedItem, setSelectedItem] = useState('')
 
   const restaurants = {
-    id: [1,2,3,4,5],
+    id: [1, 2, 3, 4, 5],
     name: ['ヨプの王豚塩焼', '常陸秋そば　善三郎', 'SHRIMP NOODLE海老ポタ', 'Button4', 'Button5'],
-    evaluation: ['3.5','3.5','4.0','',''],
-    review: ['焼肉うまし','卵はINしない方が良い。','クリーミーでうまい','','']
+    evaluation: ['3.5', '3.5', '4.0', '', ''],
+    review: ['焼肉うまし', '卵はINしない方が良い。', 'クリーミーでうまい', '', '']
   }
 
   const onOpenDialog = (name) => {
@@ -40,7 +52,7 @@ export const GurumeModal = () => {
 
   const onCloseDialog = () => {
     setSelectedItem(false)
-  }  
+  }
 
   function afterOpenModal() {
     // references are now sync'd and can be accessed.
@@ -49,35 +61,35 @@ export const GurumeModal = () => {
 
   return (
     <>
-        { restaurants.name.map((item) => {
-          return(
-            <>
-              <button onClick={ () => onOpenDialog(item)}>{item}</button>
-              <div class ="bg-black"></div>     
-             <Modal
+      {restaurants.name.map((item) => {
+        return (
+          <>
+            <button onClick={() => onOpenDialog(item)}>{item}</button>
+            <div class="bg-black"></div>
+            <Modal
               isOpen={item === selectedItem}
               onAfterOpen={afterOpenModal}
               onRequestClose={onCloseDialog}
               style={customStyles}
               contentLabel="Example Modal"
             >
-                <div class="flex place-content-between w-11/12  m-auto">
-                  <div class="text-3xl font-bold mb-2">{item}</div>
-                  <button class="font-bold" onClick={onCloseDialog}>close</button>
-                </div>
-                <img
-                    class="w-11/12 m-auto"
-                    src="https://source.unsplash.com/random/1600x900/"
-                    alt="ほげほげ画像"
-                  ></img>          
-                <p class="text-gray-700 text-base w-11/12 m-auto">
-                    <p>評価{}</p>
-                    <p>{item}</p>
-                </p>
+              <div class="flex place-content-between w-11/12  m-auto">
+                <div class="text-3xl font-bold mb-2">{item}</div>
+                <button class="font-bold" onClick={onCloseDialog}>close</button>
+              </div>
+              <img
+                class="w-11/12 m-auto"
+                src="https://source.unsplash.com/random/1600x900/"
+                alt="ほげほげ画像"
+              ></img>
+              <p class="text-gray-700 text-base w-11/12 m-auto">
+                <p>評価{titleText}</p>
+                <p>{restaurant}</p>
+              </p>
             </Modal>
-            </>
-            )          
-          })}
+          </>
+        )
+      })}
     </>
   )
 }
