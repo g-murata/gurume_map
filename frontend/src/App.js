033c7f700174d209954a9cb0eb71fdb59b510754
ta-mu-github <64738234+ta-mu-github@users.noreply.github.com>
2022-05-28 00:07:27 +0900

    ADD_ルーティング。引数を渡せるようにしたいけども今日はここまで。

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import { Main } from "./components/Main";
 import { About } from './components/About';
 import { Blog } from './components/Blog';      
 import { HogeHoge } from './components/HogeHoge';
+import {Post} from './components/blogs/Post'; 
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route exact path ="/" element={<Main />} />          
           <Route exact path ="/about" element={<About />} />
           <Route exact path ="/blog" element={<Blog />} /> 
+          <Route path ="/blog/:id" element={<Post />} />           
           <Route exact path ="/hogehoge" element={<HogeHoge />} />                   
         </Routes>
       </Router>
--- frontend/src/components/Blog.jsx
+++ frontend/src/components/Blog.jsx
@@ -1,29 +1,31 @@
 import Blogs from './../blogs.json';
+import { Link } from 'react-router-dom';
+
 const blogs = Blogs.data;
 console.log(blogs)
+
 
 export const Blog = () => {
   return (
     <div class="flex items-center flex-col max-w-screen-2xl px-4 md:px-8 mx-auto md:flex-row md:space-x-8">
       {Object.keys(blogs).map(item => (
         <>
-          <div class="max-w-sm rounded overflow-hidden shadow-lg 
-                transform hover:scale-110 transition-transform cursor-pointer"
-                onClick = {() =>alert("いつか投稿予定")}>
-          <img
-              class="w-full"
-              src={blogs[item].image}
-              alt="ほげほげ画像"
-            ></img>          
-            <div class="px-6 py-4">
-              <div class="font-bold text-xl mb-2">{blogs[item].title}</div>
-              <p class="text-gray-700 text-base">
-              <p class="text-gray-500 text-base">{blogs[item].created_at}</p>
-              </p>
+          <Link to={`/blog/${blogs[item].id}`}>                      
+            <div class="max-w-sm rounded overflow-hidden shadow-lg 
+                  transform hover:scale-110 transition-transform cursor-pointer">
+            <img
+                class="w-full"
+                src={blogs[item].image}
+                alt="ほげほげ画像"
+              ></img>
+              <div class="px-6 py-4">
+                <div class="font-bold text-xl mb-2">{blogs[item].title}</div>
+                <p class="text-gray-700 text-base">
+                <p class="text-gray-500 text-base">{blogs[item].created_at}</p>
+                </p>
+              </div>
             </div>
-        </div>
-
-                    
+          </Link>                                
         </>
       ))}    
     </div>
