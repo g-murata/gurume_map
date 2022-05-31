bf0477c99882fb4d135dc669996e3f17f9de0bb8
村田元暉 <64738234+Genki-Murata@users.noreply.github.com>
2022-05-31 17:49:11 +0900

    ADD_useParamsを使って、idを受け取れるようにする。

--- frontend/src/components/blogs/Post.jsx
+++ frontend/src/components/blogs/Post.jsx
@@ -1,7 +1,18 @@
-export const Post = (props) => {
+import { useParams } from "react-router-dom"
+
+// import Blogs from './../blogs.json';
+
+// const blogs = Blogs.data;
+// console.log(blogs)
+
+export function Post()  {
+  const { id } = useParams()
+  console.log(id)
+  
   return (
     <>
-    <div class="flex justify-center items-center">
+    <div class="flex flex-col justify-center items-center">
+      <h1>選ばれたのは{id}でした</h1>
       <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() =>alert('しばしまたれい')}>
         しばしまたれい
       </button>
