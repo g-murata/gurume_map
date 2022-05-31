63ce77f83d180afd64ce0a2bf3b53b1a5bf25fac
ta-mu-github <64738234+ta-mu-github@users.noreply.github.com>
2022-05-31 22:28:54 +0900

    ADD_ブログ部分作成

--- frontend/src/components/blogs/Post.jsx
+++ frontend/src/components/blogs/Post.jsx
@@ -1,22 +1,38 @@
 import { useParams } from "react-router-dom"
+import { Link } from "react-router-dom";
 
-// import Blogs from './../blogs.json';
+import Blogs from './../../blogs.json';
 
-// const blogs = Blogs.data;
-// console.log(blogs)
 
 export function Post()  {
   const { id } = useParams()
-  console.log(id)
+  
+  const blogs = Blogs.data;
   
   return (
     <>
     <div class="flex flex-col justify-center items-center">
-      <h1>選ばれたのは{id}でした</h1>
-      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() =>alert('しばしまたれい')}>
-        しばしまたれい
-      </button>
-    </div>
+      <div class="flex flex-col justify-center items-center">
+        <img class="w-96" src={blogs[id].image} alt="gazou"></img>
+      </div>
+
+      <div class="flex flex-col justify-center items-center">
+        <div class="px-6 py-4">
+          <div class="font-bold text-xl mb-2"> {blogs[id].title}</div>
+        </div>
+
+        <div class="text-gray-700 mb-2">
+          {blogs[id].created_at}
+        </div>
+
+        <div class="w-80 md:w-full break-words whitespace-normal text-gray-800 text-base ">
+          <p className="review">{blogs[id].contents}</p>
+        </div>
+
+      </div>    
+      <br></br>            
+      <Link to="/blog" class="text-blue-600 hover:text-red-500 active:text-yellow-700 text-lg font-semibold block">戻る</Link>
+    </div>    
     </>
   )
 }    
