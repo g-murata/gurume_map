a6ea74754b7248689559c29e6439236a6b27683d
ta-mu-g-m <jojo1234dorararara@gmail.com>
2022-07-10 09:23:53 +0900

    ADD_rails db:migrate

--- backend/db/migrate/20220708030959_create_restraunts.rb
+++ backend/db/migrate/20220708030959_create_restraunts.rb
@@ -1,6 +1,7 @@
 class CreateRestraunts < ActiveRecord::Migration[7.0]
   def change
     create_table :restraunts do |t|
+      t.string :name
 
       t.timestamps
     end
--- backend/db/migrate/20220709235826_create_blogs.rb
+++ backend/db/migrate/20220709235826_create_blogs.rb
@@ -1,6 +1,8 @@
 class CreateBlogs < ActiveRecord::Migration[7.0]
   def change
     create_table :blogs do |t|
+      t.string :title
+      t.string :content
 
       t.timestamps
     end
