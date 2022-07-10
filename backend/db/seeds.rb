8dd5d676d3081cbf3a511d6127eb3e8f9ac2f337
ta-mu-g-m <jojo1234dorararara@gmail.com>
2022-07-10 09:39:24 +0900

    ADD_初期データの作成（db:seed）

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+3.times do |n|
+  restraunts = Restraunt.new(
+    name: "テストレストラン_#{n}",
+  )
+  restraunts.save!
+
+  blogs = Blog.new(
+    title: "ほげほげ〜#{n}",
+    content: "ほげほげ_#{n}"
+  )
+  restraunts.save!  
+  blogs.save!
+  
+end
