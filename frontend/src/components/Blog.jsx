2bb0f1a287fb9b9b427b3a0cb5488e653013526d
ta-mu-g-m <jojo1234dorararara@gmail.com>
2022-07-16 10:49:22 +0900

    MOD_Railsから取得したデータを画面表示できるように。

--- frontend/src/components/Blog.jsx
+++ frontend/src/components/Blog.jsx
@@ -1,57 +1,42 @@
-import { useState, useEffect, useReducer } from 'react';
+import { useState, useEffect } from 'react';
 
-import axios from 'axios';
 import { fetchBlogs } from '../apis/blogs';
 import { Link } from 'react-router-dom';
 
 export const Blog = () => {
-  const [photo, setPhoto] = useState([])
+  const [blogs, setBlog] = useState([])
+
   useEffect(() => {
     fetchBlogs()
       .then((data) =>
-        setPhoto(data.blogs)
+        setBlog(data.blogs)
       )
   }, [])
+
   return (
-    <>
-      {console.log(photo)}
-      <h1>Hello,Rails</h1>
-      {Object.keys(photo).map(item => (
-        <>
-          {item}
-        </>
-      ))
-      }
-    </>
+    <div class="flex items-center flex-col max-w-screen-2xl px-4 md:px-8 mx-auto md:flex-row md:space-x-8">
+      <>
+        {Object.keys(blogs).map(item => (
+          <>
+            <Link to={`/blog/${blogs[item].id}`}>
+              <div class="max-w-sm rounded overflow-hidden shadow-lg 
+                  transform hover:scale-110 transition-transform cursor-pointer">
+                <img
+                  class="w-full"
+                  src={blogs[item].image}
+                  alt="ほげほげ画像"
+                ></img>
+                <div class="px-6 py-4">
+                  <div class="font-bold text-xl mb-2">{blogs[item].title}</div>
+                  <p class="text-gray-700 text-base">
+                    <p class="text-gray-500 text-base">{blogs[item].created_at}</p>
+                  </p>
+                </div>
+              </div>
+            </Link>
+          </>))
+        }
+      </>
+    </div >
   )
 }
-
-// export const Blog = () => {
-
-
-// export const Blog = () => {
-//   return (
-//     <div class="flex items-center flex-col max-w-screen-2xl px-4 md:px-8 mx-auto md:flex-row md:space-x-8">
-//       {Object.keys(blogs).map(item => (
-//         <>
-//           <Link to={`/blog/${blogs[item].id}`}>                      
-//             <div class="max-w-sm rounded overflow-hidden shadow-lg 
-//                   transform hover:scale-110 transition-transform cursor-pointer">
-//             <img
-//                 class="w-full"
-//                 src={blogs[item].image}
-//                 alt="ほげほげ画像"
-//               ></img>
-//               <div class="px-6 py-4">
-//                 <div class="font-bold text-xl mb-2">{blogs[item].title}</div>
-//                 <p class="text-gray-700 text-base">
-//                 <p class="text-gray-500 text-base">{blogs[item].created_at}</p>
-//                 </p>
-//               </div>
-//             </div>
-//           </Link>                                
-//         </>
-//       ))}    
-//     </div>
-//   )
-// }    
