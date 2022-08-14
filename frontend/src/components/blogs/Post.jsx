dbe48c90b2430a5e767488683cbf88db8b554a13
ta-mu-github <64738234+ta-mu-github@users.noreply.github.com>
2022-08-14 18:08:12 +0900

    MOD_Error fix

--- frontend/src/components/blogs/Post.jsx
+++ frontend/src/components/blogs/Post.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { useParams, useRouteMatch } from "react-router-dom"
+import { useParams } from "react-router-dom"
 import { Link } from "react-router-dom";
 
 
