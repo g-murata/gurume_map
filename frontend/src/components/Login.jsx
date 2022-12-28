899f7dca320aa375b9ecbbc3948685179e81ac94
ta-mu-github <64738234+ta-mu-github@users.noreply.github.com>
2022-12-28 20:35:13 +0900

    ADD_ログインエラー作成

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -3,15 +3,37 @@ import { signInWithEmailAndPassword } from 'firebase/auth';
 import { signOut } from 'firebase/auth';
 import { useNavigate, Link } from 'react-router-dom';
 import { useAuthContext } from '../context/AuthContext';
+import { useState } from 'react';
 
 export const Login = () => {
   const { user } = useAuthContext();
   const navigate = useNavigate();
+  const [error, setError] = useState('');
 
   const handleSubmit = (event) => {
     event.preventDefault();
     const { email, password } = event.target.elements;
-    signInWithEmailAndPassword(auth, email.value, password.value);
+    signInWithEmailAndPassword(auth, email.value, password.value)
+      .then(() => {
+        navigate('/');
+      })
+      .catch((error) => {
+        switch (error.code) {
+          case 'auth/invalid-email':
+            setError('正しいメールアドレスの形式で入力してください。');
+            break;
+          case 'auth/user-not-found':
+            setError('メールアドレスかパスワードに誤りがあります。');
+            break;
+          case 'auth/wrong-password':
+            setError('メールアドレスかパスワードに誤りがあります。');
+            break;
+          default:
+            setError('メールアドレスかパスワードに誤りがあります。');
+            break;
+        }
+      }
+      )
   };
 
   const handleLogout = () => {
@@ -29,7 +51,7 @@ export const Login = () => {
   }else{
   return (
     <div>
-      <h1 class="text-red-600 active:text-yellow-700 text-lg font-semibold block">ただいま開発中！！！！※トップページ行こうとするとloginページにリダイレクトされるけど開発中につき！</h1>
+      {error && <p style={{ color: 'red' }}>{error}</p>}
       <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="email">メールアドレス</label>
@@ -51,7 +73,9 @@ export const Login = () => {
           ユーザ登録は<Link to={'/signup'}>こちら</Link>から
         </div>
       </form>
+      <h1 class="text-blue-500 active:text-yellow-700 text-lg font-semibold block">ただいま開発中！！！！※トップページ行こうとするとloginページにリダイレクトされるけど開発中につき！</h1>
     </div>
+
   );
   }
 }
