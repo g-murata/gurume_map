4b4d5199c2b03ca58d947119431b3d24557429ae
ta-mu-g-m <jojo1234dorararara@gmail.com>
2022-12-31 17:56:56 +0900

    ADD_レストランモデルにユーザモデルのアソシエーションを追加

--- backend/app/models/restraunt.rb
+++ backend/app/models/restraunt.rb
@@ -1,4 +1,7 @@
 class Restraunt < ApplicationRecord
+
+  belongs_to :user
+
   validates :name, presence: true, length: { maximum: 100 }
   validates :evaluation, presence: true, numericality: {greater_than: 0,less_than_or_equal_to: 5}
   validates :review, presence: true, length: { maximum: 1000 }
