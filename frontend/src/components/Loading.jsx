742c7eea41f2f2b874c322852b7ec80c0a72697c
ta-mu-g-m <jojo1234dorararara@gmail.com>
2023-02-04 13:06:55 +0900

    Update Loading.jsx

--- frontend/src/components/Loading.jsx
+++ frontend/src/components/Loading.jsx
@@ -11,7 +11,6 @@ function Loading() {
     return (
         <div className="z-10 h-screen w-screen flex justify-center items-center" style={customStyles}>
             <div className="animate-spin h-10 w-10 border-4 border-blue-500 rounded-full border-t-transparent"></div>
-            <h1>ローディングなう</h1>
         </div>
     )
 }
