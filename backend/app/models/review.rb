23e7343e9cdfcdd58350c311b0eeb5bc208c09bb
ta-mu-github <64738234+ta-mu-github@users.noreply.github.com>
2023-02-11 07:17:28 +0900

    ADD_レビューモデルとユーザモデルに1対1の関係を付与

--- backend/app/models/review.rb
+++ backend/app/models/review.rb
@@ -1,5 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :restraunt
+  belongs_to :user
 
   validates :evaluation, presence: true, numericality: {greater_than: 0,less_than_or_equal_to: 5}
   validates :content, presence: true, length: { maximum: 1000 }
