33a594b8848a68449647ea91ad510c51cfe05f54
ta-mu-github <64738234+ta-mu-github@users.noreply.github.com>
2023-02-25 09:10:23 +0900

    ADD_試しに新規店名登録モーダルをコンポーネント化

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -15,6 +15,7 @@ import {
 import Modal from 'react-modal';
 import ReactStarsRating from 'react-awesome-stars-rating';
 import Loading from './Loading';
+import CreateRestrauntModal from './CreateRestrauntModal';
 
 
 // import Restaurants from './../restaurants.json';
@@ -446,34 +447,11 @@ export const Main = () => {
         >
           {/* todo: */}
           <form onSubmit={handleSubmit}>
-            <div className="max-w-lg px-8 mx-auto md:px-8 md:flex-row">
-              <div className="text-3xl font-bold text-center">
-                新規店名登録
-              </div>
-              {error && <p style={{ color: 'red' }}>{error}</p>}
-              <div className="text-right">
-                <button className="font-bold" onClick={closeModal}>Close</button>
-              </div>
-              <label className="block text-gray-700 text-sm font-bold mb-2" for="name">
-                店名
-              </label>
-              <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="name" placeholder="店名" name="name" />
-              <div>
-                {/* <label for="lat" className="block text-gray-700 text-sm font-bold mb-2">
-                  経緯
-                </label> */}
-                <input type="hidden" id="lat" name="lat" rows="4" readonly="true" className="bg-slate-400 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" value={coordinateLat}></input>
-              </div>
-              <div>
-                {/* <label for="lng" className="block text-gray-700 text-sm font-bold mb-2">
-                  経度
-                </label> */}
-                <input type="hidden" id="lng" name="lng" rows="4" readonly="true" className="bg-slate-400 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" value={coordinateLng}></input>
-              </div>
-              <div className='flex justify-center '>
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 my-6 rounded-full">登録</button>
-              </div>
-            </div>
+            <CreateRestrauntModal 
+              error={error}
+              coordinateLat={coordinateLat}
+              coordinateLng={coordinateLng}
+            />
           </form>
         </Modal>
       </LoadScript >
