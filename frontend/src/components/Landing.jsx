03b9a4a05e36a29e8135818f37eaed2958fa0803
ta-mu-github <64738234+ta-mu-github@users.noreply.github.com>
2023-03-18 19:02:46 +0900

    MOD＿画像

--- frontend/src/components/Landing.jsx
+++ frontend/src/components/Landing.jsx
@@ -3,12 +3,12 @@ export const Landing = () => {
     <>
       <div className="max-w-screen-2xl px-4 md:px-8 mx-auto">
         <div className="flex items-center flex-col"> 
-          <img src={`${process.env.PUBLIC_URL}/GurumeMap.jpg`} className="w-2/4" alt="Logo" />
-          <img src={`${process.env.PUBLIC_URL}/HowToUse.jpg`} className="w-2/4" alt="Logo" />
+          <img src={`${process.env.PUBLIC_URL}/GurumeMap.jpg`} className="md:w-2/4" alt="GurumeMap" />
+          <img src={`${process.env.PUBLIC_URL}/HowToUse.jpg`} className="md:w-2/4" alt="HowToUse" />
         </div>
           <div className="flex items-center flex-col md:flex-row">
-            <img src={`${process.env.PUBLIC_URL}/HowToUse_1.jpg`} className="w-2/4" alt="Logo" />
-            <img src={`${process.env.PUBLIC_URL}/HowToUse_2.jpg`} className="w-2/4" alt="Logo" />
+            <img src={`${process.env.PUBLIC_URL}/HowToUse_1.jpg`} className="md:-2/4" alt="HowToUse_1" />
+            <img src={`${process.env.PUBLIC_URL}/HowToUse_2.jpg`} className="md:w-2/4" alt="HowToUse_2" />
           </div>
       </div>
     </>
