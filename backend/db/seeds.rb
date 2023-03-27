ece472ee55c1eea1aa12df06a2ffd1efa411f9ab
ta-mu-github <64738234+ta-mu-github@users.noreply.github.com>
2023-03-27 20:45:27 +0900

    MOD_テストデータ修正

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -7,20 +7,21 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 user = User.create(name: "ゲスト太郎", email: "guest@guest.co.jp", password: "uso_no_password")
-user2 = User.create(name: "テスト太郎", email: "testuser@test.co.jp", password: "uso_no_password")
+user1 = User.create(name: "テスト太郎１", email: "testuser@test.co.jp", password: "uso_no_password")
+user2 = User.create(name: "ほげほげ君", email: "hogehoge@hoge.co.jp", password: "uso_no_password")
 
 restraunts = Restraunt.create(
   name: "テストレストラン",
   image: "https://2.bp.blogspot.com/-NSxv59ZcJfA/VpjCbp0555I/AAAAAAAA3AM/jVD3WGXyRlU/s800/group_kids.png",
   lat: "35.66587105141782",
   lng: "139.7545815170528",
-  user_id: user.id,
+  user_id: user1.id,
 )
 
 review = Review.create(
   evaluation: 3.5,
   content: "うまーーい。",
-  user_id: user.id,
+  user_id: user1.id,
   restraunt_id: restraunts.id
 )
 review = Review.create(
