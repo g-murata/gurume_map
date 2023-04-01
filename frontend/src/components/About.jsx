1036446fe9a8166ca3288d029768bc461bf7833a
ta-mu-g-m <jojo1234dorararara@gmail.com>
2023-04-01 14:45:28 +0900

    Update About.jsx

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -3,11 +3,11 @@ export const About = () => {
     <>
       <div className="max-w-screen-2xl px-4 md:px-8 mx-auto">
         <div className="md:px-3 p-3 ">
-          <h1 className="text-lg bg-green-300">GurumeMapとは？</h1>
           <div className="md:text-lg text-gray-600 text-sm">
-            <p>新橋の飲食店を、</p>
-            <p>私村田が独断と偏見でレビューしたサイトです。</p>
-            <p>評価は5段階となっております。</p>
+            <h1 className="text-lg bg-green-300">GurumeMapとは？</h1>
+            <p>新橋付近の飲食店のレビューをすることができるアプリです。</p>
+            <p>レビューを投稿する場合は新規会員登録が必要です。</p>
+            <p>※会員登録しなくても、ゲストユーザでログインすればレビューを閲覧することだけ可能。</p>
             <br></br>
           </div>
           <h1 className="text-lg bg-orange-300">使用している技術</h1>
@@ -16,11 +16,14 @@ export const About = () => {
             <p>CSSフレームワーク：Tailwind CSS</p>
             <p>バックエンド：Ruby on Rails</p>
             <p>データベース：PostgreSQL</p>
-            <p>ユーザ認証機能：Firebase Authentication</p>            
+            <p>ユーザ認証機能：Firebase Authentication</p>
             <br></br>
             <p>＜デプロイ先＞</p>
             <p>フロントエンド：netlify</p>
-            <p>バックエンド：heroku→無料プラン終了につき移行先検討中</p>
+            <p>バックエンド：heroku → fly.io（Herokuが無料プランを終了したため。）</p>
+          </div>
+          <div className="flex items-center flex-col">
+            <img src={`${process.env.PUBLIC_URL}/Technology.jpg`} className="md:w-2/4" alt="HowToUse_1" />
           </div>
         </div>
       </div>
