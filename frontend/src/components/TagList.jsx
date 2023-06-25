eaa40f68b58122ea6c9e0ebb027987bb2063cce4
ta-mu-github <64738234+ta-mu-github@users.noreply.github.com>
2023-06-25 12:31:19 +0900

    MOD_スマホ用に文字サイズ調整

--- frontend/src/components/TagList.jsx
+++ frontend/src/components/TagList.jsx
@@ -8,7 +8,7 @@ export const TagList = (props) => {
             {/* TODO: undefined判定については、開発環境限定の処理にしてもよいかもしれんね？ */}
             {props.tags.find(tag => tag.id === props.tags_tagged_items[key].tag_id) !== undefined && 
               <div className="bg-gray-100 font-bold py-2 px-4 m-1 rounded-full inline-block">
-                <p className="text-gray-600">
+                <p className="text-gray-600 text-sm md:text-base">
                   {props.tags.find(tag => tag.id === props.tags_tagged_items[key].tag_id).name}
                 </p>
               </div>
