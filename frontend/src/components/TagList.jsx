6e753143447cbe86e09d5e42ff7bf02a6f7e1401
ta-mu-github <64738234+ta-mu-github@users.noreply.github.com>
2023-06-25 12:18:46 +0900

    バグ修正

--- frontend/src/components/TagList.jsx
+++ frontend/src/components/TagList.jsx
@@ -6,7 +6,7 @@ export const TagList = (props) => {
           return(
           <>
             {/* TODO: undefined判定については、開発環境限定の処理にしてもよいかもしれんね？ */}
-            {props.tags[props.tags_tagged_items[key].tag_id] !== undefined && 
+            {props.tags.find(tag => tag.id === props.tags_tagged_items[key].tag_id) !== undefined && 
               <div className="bg-gray-100 font-bold py-2 px-4 m-1 rounded-full inline-block">
                 <p className="text-gray-600">
                   {props.tags.find(tag => tag.id === props.tags_tagged_items[key].tag_id).name}
