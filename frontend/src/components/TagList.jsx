7b2661f0d685183c1056be7e19a5a08f455b69c6
ta-mu-github <64738234+ta-mu-github@users.noreply.github.com>
2023-06-25 11:57:33 +0900

    Update TagList.jsx

--- frontend/src/components/TagList.jsx
+++ frontend/src/components/TagList.jsx
@@ -5,6 +5,7 @@ export const TagList = (props) => {
       {(Object.keys(props.tags_tagged_items)).map(key => {
           return(
           <>
+            {/* TODO: undefined判定については、開発環境限定の処理にしてもよいかもしれんね？ */}
             {props.tags[props.tags_tagged_items[key].tag_id] != undefined && 
               <div className="bg-gray-100 font-bold py-2 px-4 m-1 rounded-full inline-block">
                 <p className="text-gray-600">
