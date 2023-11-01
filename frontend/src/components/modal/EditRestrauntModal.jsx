e6362fa3d0f0485ae228b17129293b92c5fed8d9
ta-mu-github <64738234+ta-mu-github@users.noreply.github.com>
2023-11-01 20:43:24 +0900

    ADD_編集画面を開いたとき、選択されたタグが選択されているようにしたい。

--- frontend/src/components/modal/EditRestrauntModal.jsx
+++ frontend/src/components/modal/EditRestrauntModal.jsx
@@ -1,6 +1,20 @@
+import { useState } from "react";
 import {TagList} from '../TagList';
 
 export const EditRestrauntModal = (props) => {
+  const [isSelected, setIsSelected] = useState(false);
+  const [selectedTags, setSelectedTags] = useState([]);
+
+  const handleTagClick = (tagId) => {
+    // 選択されたタグを追加または削除する処理
+    setIsSelected(!isSelected)
+    if (selectedTags.includes(tagId)) {
+      setSelectedTags(selectedTags.filter((id) => id !== tagId));
+    } else {
+      setSelectedTags([...selectedTags, tagId]);
+    }
+  };
+
   return (
     <>
       <div className="max-w-lg px-8 mx-auto md:px-8 md:flex-row">
@@ -29,10 +43,25 @@ export const EditRestrauntModal = (props) => {
           <input type="hidden" id="lng" name="lng" rows="4" readonly="true" className="bg-slate-400 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" value={props.restaurant.lng}></input>
         </div>
 
-        <TagList 
-            tags_tagged_items={props.tags_tagged_items}
-            tags={props.tags}
-          />
+        <div className="my-2">                           
+          {Object.keys(props.tags).map(item => {
+            console.log(props.tags_tagged_items)
+            console.log(props.tags[item].id)
+            console.log(selectedTags)
+            return (
+              <>
+                <button 
+                  className={`bg-blue-500 text-white font-bold mx-2 px-2 rounded ${selectedTags.includes(props.tags[item].id) ? 'bg-red-500' : ''}`} 
+                  key={props.tags[item].id} 
+                  onClick={() => handleTagClick(props.tags[item].id)}
+                >
+                  {props.tags[item].name}
+                </button >  
+              </>
+            )}
+            )
+          }
+        </div>
 
         <div className='flex justify-center '>
           <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 my-6 rounded-full">更新</button>
