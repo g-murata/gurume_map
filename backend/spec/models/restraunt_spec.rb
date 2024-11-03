a2d7c0389523220d4dbb88c0be378f655fd7e3bd
ta-mu-github <64738234+ta-mu-github@users.noreply.github.com>
2024-11-03 20:40:34 +0900

    ADD_RSpecテストケース作成

--- backend/spec/models/restraunt_spec.rb
+++ backend/spec/models/restraunt_spec.rb
@@ -6,23 +6,29 @@ RSpec.describe Restraunt, type: :model do
     expect(restraunt).to be_valid
   end
 
-  context "店名がnilの場合" do
-    specify "異常" do
-      restraunt = FactoryBot.build(:restraunt)
-      restraunt.name = nil
-      expect(restraunt).not_to be_valid
-      expect(restraunt.errors[:name].size).to eq(1)
-      expect(restraunt.errors[:name]).to include("can't be blank") 
-    end
-  end  
+  specify "店名がnilの場合、異常" do
+    restraunt = FactoryBot.build(:restraunt)
+    restraunt.name = nil
+    expect(restraunt).not_to be_valid
+    expect(restraunt.errors[:name].size).to eq(1)
+    expect(restraunt.errors[:name]).to include("can't be blank") 
+  end
 
-  # context "説明がnilの場合" do
-  #   specify "異常" do
-  #     restraunt = FactoryBot.build(:restraunt)
-  #     restraunt.description = nil
-  #     expect(restraunt).not_to be_valid
-  #     expect(restraunt.errors[:description].size).to eq(1)
-  #     expect(restraunt.errors[:description]).to include("can't be blank") 
-  #   end
-  # end  
+  specify "店名は100文字以内" do
+    restraunt = FactoryBot.build(:restraunt)
+    restraunt.name = "a" * 101
+    expect(restraunt).not_to be_valid
+    expect(restraunt.errors[:name].size).to eq(1)
+    expect(restraunt.errors[:name]).to include("is too long (maximum is 100 characters)") 
+    restraunt.name = "a" * 100
+    expect(restraunt).to be_valid    
+  end
+
+  specify "説明は100文字以内" do
+    restraunt = FactoryBot.build(:restraunt)
+    restraunt.description = "a" * 101
+    expect(restraunt).not_to be_valid
+    expect(restraunt.errors[:description].size).to eq(1)
+    expect(restraunt.errors[:description]).to include("is too long (maximum is 100 characters)") 
+  end
 end
