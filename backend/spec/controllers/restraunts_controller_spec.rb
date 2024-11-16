fed8ca6295d1576cb61426c43edb4a3d564bb106
g-murata <jojo1234dorararara@gmail.com>
2024-11-16 11:36:34 +0900

    ADD_テストコードの追加

--- backend/spec/controllers/restraunts_controller_spec.rb
+++ backend/spec/controllers/restraunts_controller_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::RestrauntsController, type: :request  do
   let(:restaurant) { create(:restraunt) } 
+
+  # 一覧
   describe "GET #index" do
     it "returns a successful response" do
       restaurant
@@ -27,21 +29,76 @@ RSpec.describe Api::V1::RestrauntsController, type: :request  do
   #   end
   # end
 
-  # describe "POST #create" do
-  #   context "with valid attributes" do
-  #     it "creates a new post" do
-  #       expect {
-  #         post :create, params: { post: attributes_for(:post) }
-  #       }.to change(Post, :count).by(1)
-  #     end
-  #   end
+  # 登録
+  describe "POST #create" do
+    let(:valid_attributes) { { name: 'テストなお店', description: 'Postのテスト', user: create(:user) } }
+    let(:invalid_attributes) { { name: '', description: '', user_id: '' } }
 
-  #   context "with invalid attributes" do
-  #     it "does not create a new post" do
-  #       expect {
-  #         post :create, params: { post: attributes_for(:post, title: nil) }
-  #       }.not_to change(Post, :count)
-  #     end
-  #   end
-  # end
+    context "with valid attributes" do
+      it "creates a new post" do
+        expect {
+          post "/api/v1/restraunts", params: { restraunt: valid_attributes }
+        }.to change(Restraunt, :count).by(1)
+      end
+    end
+
+    context "with invalid attributes" do
+      it "does not create a new post" do
+        expect {
+          post "/api/v1/restraunts", params: { your_model: invalid_attributes }
+        }.to_not change(Restraunt, :count)
+        
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+  end
+
+
+  # 更新
+  describe "PATCH /api/v1/restraunts/:id" do
+    # let(:existing_record) { YourModel.create(attribute1: 'old_value', attribute2: 'old_value2') }
+    let(:valid_attributes) { { name: 'テストなお店', description: 'Postのテスト'} }
+    let(:invalid_attributes) { { name: '', description: ''} }
+
+    context "with valid parameters" do
+      it "updates the record" do
+        patch "/api/v1/restraunts/#{restaurant.id}", params: { your_model: valid_attributes }
+
+        expect(response).to have_http_status(:ok)
+        expect(restaurant.reload.name).to eq('テストなお店')
+      end
+    end
+
+    context "with invalid parameters" do
+      it "does not update the record" do
+        patch "/api/v1/restraunts/#{restaurant.id}", params: { your_model: invalid_attributes }
+
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(restaurant.reload.name).to eq(restaurant.name) # 変更されないことを確認
+      end
+    end
+  end
+
+
+  # 削除
+  describe "DELETE /api/v1/restraunts/:id" do
+    # let!(:record_to_delete) { YourModel.create(attribute1: 'value1', attribute2: 'value2') }
+
+    it "deletes the record" do
+      expect {
+        delete "/api/v1/restraunts/#{restaurant.id}"
+      }.to change(Restraunt, :count).by(-1)
+
+      expect(response).to have_http_status(:no_content)
+    end
+
+    it "returns not found for non-existing record" do
+      expect {
+        delete "/api/v1/restraunts/1000" # 存在しないID
+      }.to_not change(Restraunt, :count)
+
+      expect(response).to have_http_status(:not_found)
+    end
+  end
+
 end
