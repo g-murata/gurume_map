59e29c5068e152174eca519f8c2fdb76a6608913
ta-mu-github <64738234+ta-mu-github@users.noreply.github.com>
2024-12-14 12:18:11 +0900

    ADD_areasテーブル

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_06_111937) do
+ActiveRecord::Schema[7.0].define(version: 2024_12_14_031538) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "areas", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
