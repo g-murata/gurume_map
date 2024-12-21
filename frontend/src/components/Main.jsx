cfecaa7db7210af1fdec7e53bf1c3e3ef8ff9ab7
ta-mu-github <64738234+ta-mu-github@users.noreply.github.com>
2024-12-21 21:45:38 +0900

    Update Main.jsx

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -577,8 +577,8 @@ export const Main = (props) => {
                   </InfoWindow>
                 )              
               }
-              <Marker icon={{ url: `${process.env.PUBLIC_URL}/ishii_marker.png` }}
-                position={areas[Number(0)]} button onClick={() => alert('石井ビル')} />
+              {/* <Marker icon={{ url: `${process.env.PUBLIC_URL}/ishii_marker.png` }}
+                position={areas[Number(0)]} button onClick={() => alert('石井ビル')} /> */}
 
             </GoogleMap>
           </div>
